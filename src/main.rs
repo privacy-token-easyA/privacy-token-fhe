eb2c668ac2a518a7a6c2e4499f5902ae8593bfa0
shankars99 <shankars0899@gmail.com>
2023-06-17 19:55:31 -0400

    tests pass

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#![allow(unused_imports, unused_variables, dead_code)]
+
+use fhe::bfv::{BfvParametersBuilder, Ciphertext, Encoding, Plaintext, PublicKey, SecretKey};
+use fhe_traits::*;
+use rand::{rngs::OsRng, thread_rng};
+use std::sync::Arc;
+
+mod fhe_account_handler {
+    pub(crate) mod get_keys;
+    pub(crate) mod user;
+}
+
+mod fhe_node {
+    pub(crate) mod fhe_execution;
+    pub(crate) mod fhe_oracle;
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test() {
+        assert_eq!(1, 1);
+    }
 }
