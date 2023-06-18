401d95abf2137f167ab93283dc5b64405f8fa71e
shankars99 <shankars0899@gmail.com>
2023-06-18 08:07:36 -0400

    changed the API structs for each type of tx

--- src/rocket_helper/structs.rs
+++ src/rocket_helper/structs.rs
@@ -1,33 +1,45 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize)]
-pub struct Message {
+pub struct MessageApi {
     pub message: &'static str,
 }
 
 #[derive(Deserialize, Serialize, Clone)]
-pub struct DepositFunds {
-    pub address: String,
+pub struct OracleUserApi {
     pub amount: String,
+    pub sender_address: String,
+    pub receiver_address: String,
     pub der_key: String,
-}
-
-#[derive(Deserialize, Serialize, Clone)]
-pub struct SendFunds {
-    pub address_from: String,
-    pub address_to: String,
-    pub amount: String, // String?
-}
-
-#[derive(Deserialize, Serialize, Clone)]
-pub struct WithdrawFunds {
-    pub address_to: String,
-    pub amount: String, // String?
-}
-
-#[derive(Deserialize, Serialize, Clone)]
-pub struct OracleUserString {
-    pub address: String,
     pub fhe_pk: String,
     pub fhe_balance: String,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct ResponseApi {
+    pub res: String,
+    pub res_status: String,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct DepositFundsApi {
+    pub address: String,
+    pub amount: String,
+    pub fhe_pk: String,
+    pub fhe_balance: String,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct SendFundsApi {
+    pub fhe_tx_sender: String,
+    pub fhe_tx_receiver: String,
+    pub fhe_proof: String,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct WithdrawFundsApi {
+    pub amount: String,
+    pub fhe_sk: String,
+    pub fhe_pk_new: String,
+    pub fhe_balance_new: String,
 }
