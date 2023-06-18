5e42d44d9b955f89b66f33d3a70e484c618cea03
shankars99 <shankars0899@gmail.com>
2023-06-18 04:48:47 -0400

    withdraw works

--- src/fhe_tx_sender/tx_sender.rs
+++ src/fhe_tx_sender/tx_sender.rs
@@ -1,5 +1,5 @@
 use ethers::abi::{decode, encode, Token};
-use fhe::bfv::{Ciphertext, Plaintext, PublicKey};
+use fhe::bfv::{Ciphertext, Plaintext, PublicKey, SecretKey};
 use fhe_traits::Serialize;
 use std::process::Output;
 use std::str;
@@ -45,15 +45,6 @@ async fn deposit_tokens_tx_sender(
     }
 }
 
-/*
-function recvTx(
-        string calldata _to,
-        bytes calldata _fhe_tx_sender,
-        bytes calldata _fhe_tx_receiver,
-        bytes calldata _fhe_proof
-    )
-*/
-
 pub async fn send_fhe_tx(
     fhe_tx_sender: &str,
     fhe_tx_receiver: &str,
@@ -70,6 +61,48 @@ pub async fn send_fhe_tx(
         .arg(fhe_tx_sender)
         .arg(fhe_tx_receiver)
         .arg(fhe_proof)
+        .arg("--private-key")
+        .arg(priv_key)
+        .arg("--value")
+        .arg(amount)
+        .output()
+        .await?;
+
+    match get_tx_hash(output).await {
+        Ok(tx_hash) => Ok(tx_hash),
+        Err(error) => {
+            eprintln!("Failed to execute script: {}", error);
+            Ok(None)
+        }
+    }
+}
+
+pub async fn withdraw_ETH_request(
+    amount: &String,
+    fhe_sk: &SecretKey,
+    fhe_new_pk: &PublicKey,
+    fhe_balance: &Ciphertext,
+    priv_key: &String,
+) -> Result<Option<String>, Box<dyn std::error::Error>> {
+    let deployed_address = get_deployed_address();
+
+    let sk_bytes = fhe_sk.to_bytes().to_vec();
+    let sk_encoded = Token::Bytes(sk_bytes).to_string();
+
+    let pk_bytes = fhe_new_pk.to_bytes();
+    let pk_new_encoded = Token::Bytes(pk_bytes).to_string();
+
+    let fhe_balance_bytes = fhe_balance.to_bytes();
+    let fhe_balance_encoded = Token::Bytes(fhe_balance_bytes).to_string();
+
+    let output = Command::new("cast")
+        .arg("send")
+        .arg(deployed_address)
+        .arg("withdraw_ETH_request(uint256,string,string,string)")
+        .arg(amount)
+        .arg(sk_encoded)
+        .arg(pk_new_encoded)
+        .arg(fhe_balance_encoded)
         .arg("--private-key")
         .arg(priv_key)
         .arg("--value")
@@ -171,6 +204,26 @@ mod tests {
             &FEE.to_string(),
         )
         .await;
+
+        assert!(tx_hash.is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_withdraw_ETH_request() {
+        let rng = thread_rng();
+
+        let (fhe_oracle, alice, bob, owner) = create_users(100, 50);
+
+        let priv_key = get_keys("owner").unwrap().private_key.to_string();
+        let fhe_sk = owner.fhe_sk.clone();
+        let fhe_pk = alice.fhe_pk.clone();
+        let fhe_balance = owner.fhe_balance.clone();
+
+        let tx_hash =
+            deposit_tokens_tx_sender(&fhe_pk, &priv_key, &fhe_balance, &FEE.to_string()).await;
+
+        let tx_hash =
+            withdraw_ETH_request(&FEE.to_string(), &fhe_sk, &fhe_pk, &fhe_balance, &priv_key).await;
 
         assert!(tx_hash.is_ok());
     }
